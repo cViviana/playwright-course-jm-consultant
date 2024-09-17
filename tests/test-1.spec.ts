b139766c30066165d02b7ed27a664b873ed0e6c5
cViviana <leidyv@unicauca.edu.co>
2024-09-16 22:44:27 -0500

    Comentario Test 1

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -5,8 +5,8 @@ test('test', async ({ page }) => {
   await page.getByPlaceholder('Buscar productos, marcas y má').click();
   await page.getByPlaceholder('Buscar productos, marcas y má').fill('iphone');
   await page.getByPlaceholder('Buscar productos, marcas y má').press('Enter');
-  await page.getByRole('link', { name: 'Apple iPhone 11 (128 GB) -' }).click();
-  await page.getByRole('button', { name: 'Comprar ahora' }).click();
+  await page.getByRole('link', { name: 'Apple iPhone 15 (128 GB) - Negro' }).click();
+  //await page.getByRole('button', { name: 'Comprar ahora' }).click();
   
   
 });  
